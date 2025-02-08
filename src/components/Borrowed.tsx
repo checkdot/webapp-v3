9e199fb3b3189cbafec9f164a637269003864296
jode-vri <jode-vri@student.42.fr>
2025-02-08 03:02:13 +0100

    Add borrowed and deposited assets components with asset details

--- src/components/Borrowed.tsx
+++ src/components/Borrowed.tsx
@@ -2,20 +2,43 @@ import { useAccount } from 'wagmi';
 
 import './Accounts.scss';
 
+const borrowedAssets = [
+    {
+        symbol: 'USDS',
+        icon: '/icons/usds.png',
+        price: '1.00',
+        borrows: {
+            amount: '5.0180',
+            value: '5.03'
+        }
+    }
+];
+
 const Borrowed = () => {
     const { isConnected } = useAccount();
 
     if (!isConnected) {
-        return (
-            <></>
-        );
+        return null;
     }
 
     return (
         <div className="accounts-panel">
-            <h2>Borrowed Assets</h2>
+            <h2>Borrowed assets</h2>
             <div className="account-info">
-                Borrowed
+                <div className="accounts-list">
+                    {borrowedAssets.map((asset) => (
+                        <div key={asset.symbol} className="account-item">
+                            <div className="account-item-left">
+                                <img src={asset.icon} alt={asset.symbol} />
+                                <span>{asset.symbol}</span>
+                            </div>
+                            <div className="account-item-right">
+                                <div className="balance">{asset.borrows.amount} {asset.symbol}</div>
+                                <div className="usd-value">${asset.borrows.value}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/components/Deposited.tsx
+++ src/components/Deposited.tsx
@@ -1,21 +1,43 @@
 import { useAccount } from 'wagmi';
-
 import './Accounts.scss';
+
+const depositedAssets = [
+    {
+        symbol: 'USDS',
+        icon: '/icons/usds.png',
+        price: '1.00',
+        deposits: {
+            amount: '10.0180',
+            value: '10.03'
+        }
+    }
+];
 
 const Deposited = () => {
     const { isConnected } = useAccount();
 
     if (!isConnected) {
-        return (
-            <></>
-        );
+        return null;
     }
 
     return (
         <div className="accounts-panel">
-            <h2>Deposited Assets</h2>
+            <h2>Deposited assets</h2>
             <div className="account-info">
-                Deposited
+                <div className="accounts-list">
+                    {depositedAssets.map((asset) => (
+                        <div key={asset.symbol} className="account-item">
+                            <div className="account-item-left">
+                                <img src={asset.icon} alt={asset.symbol} />
+                                <span>{asset.symbol}</span>
+                            </div>
+                            <div className="account-item-right">
+                                <div className="balance">{asset.deposits.amount} {asset.symbol}</div>
+                                <div className="usd-value">${asset.deposits.value}</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
